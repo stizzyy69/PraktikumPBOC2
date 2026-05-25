792a15670a5ecbec955ae393e0eaa7ce9db8e90a
stizzyy <71333597+stizzyy69@users.noreply.github.com>
2026-05-25 17:40:00 +0700

    Add files via upload

---
+++ PBO10_24060124130080/jdbc_mhs_service/src/jdbc/program/Program.java
@@ -0,0 +1,13 @@
+package jdbc.program;
+
+import jdbc.utilities.MysqlUtility;
+
+/**
+ *
+ * @author sirie
+ */
+public class Program {
+    public static void main(String[] args) {
+        MysqlUtility.getConnection();
+    }
+}
